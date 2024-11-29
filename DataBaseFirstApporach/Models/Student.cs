1d30c06021b22ea47253f85653665d0c065a9074
Aman gwari <amangwari29sep@gmail.com>
2024-11-30 01:20:00 +0530

    Add project files.

---
+++ DataBaseFirstApporach/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DataBaseFirstApporach.Models;
+
+public partial class Student
+{
+    public int Id { get; set; }
+
+    public string StudentName { get; set; } = null!;
+
+    public string StudentGender { get; set; } = null!;
+
+    public int Age { get; set; }
+
+    public int Standard { get; set; }
+
+    public string Gurdian { get; set; } = null!;
+}
